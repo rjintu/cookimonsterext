2ca870f1764f911aa251a07fbb3cd82a1f842e60
Rohan Jinturkar <rjintu@users.noreply.github.com>
2022-04-19 02:45:17 -0400

    testing with cookies

--- background.js
+++ background.js
@@ -3,6 +3,10 @@
 let color = '#3aa757';
 console.log('Got this far');
 
+chrome.cookies.onChanged.addListener(function(info) {
+  console.log("onChanged" + JSON.stringify(info));
+});
+
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
--- popup.js
+++ popup.js
@@ -1,24 +1,46 @@
-// Initialize butotn with users's prefered color
-let changeColor = document.getElementById("changeColor");
+// shortcut for cookies
+if (!chrome.cookies) {
+  chrome.cookies = chrome.experimental.cookies;
+}
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
+// Initialize button with users's preferred color
+let listCookies = document.getElementById("listCookies");
+
+
+  // listCookies.style.backgroundColor = color;
+//   console.log(chrome.cookies)
+// });
 
 // When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
+listCookies.addEventListener("click", async () => {
+  console.log("clicked");
+  chrome.tabs.query({"status":"complete","windowId":chrome.windows.WINDOW_ID_CURRENT,"active":true}, function(tab){
+    console.log(JSON.stringify(tab));
+    chrome.cookies.getAll({"url":tab[0].url}, function(cookies) {
+      console.log(cookies.length);
+      all_cookies = "";
+      for (var elem in cookies) {
+        var curr = JSON.stringify(cookies[elem]);
+        console.log(curr);
+        all_cookies = all_cookies + curr;
+    }
+    localStorage.all_cookies = all_cookies;
+    console.log("showed cookies");
   });
+});
+
+  // let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  // chrome.scripting.executeScript({
+  //   target: { tabId: tab.id },
+  //   function: setPageBackgroundColor,
+  // });
 });
 
 // The body of this function will be execuetd as a content script inside the
 // current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
-}
+// function setPageBackgroundColor() {
+//   chrome.storage.sync.get("color", ({ color }) => {
+//     document.body.style.backgroundColor = color;
+//   });
+// }
