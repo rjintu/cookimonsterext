c28a6ef513c7228ed879e6ad21844bb5031e1a3a
Rohan Jinturkar <rjintu@users.noreply.github.com>
2022-04-26 17:16:18 -0400

    more code cleanup

--- background.js
+++ background.js
@@ -1,41 +1,5 @@
 // https://developer.chrome.com/docs/extensions/mv3/getstarted/
 
-let color = '#3aa757';
-
-// chrome.cookies.onChanged.addListener(function(info) {
-//   console.log("onChanged" + JSON.stringify(info));
-// });
-
-let unique_domains; 
-
-
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  // document.getElementById("number").innerHTML = Math.floor(Math.random(10) * 10)
-  let test_var = Math.floor(Math.random(10) * 10)
-
-  chrome.runtime.sendMessage({greeting: test_var}, function(response) {
-    console.log(response.farewell);
-});
-  chrome.cookies.getAll({
-    }, function (theCookies) {
-        var domains = [];
-        cookies = theCookies;
-        var counter = 0;
-        console.log(cookies.length);
-        for (var i in cookies) { // iterates through the indices!! not a for each loop
-          // console.log(cookies[i]);
-          // console.log(cookies[i].domain);
-          domains.push(cookies[i].domain);
-          counter += 1;
-        }
-        // console.log(counter);
-        unique_domains = [...new Set(domains)];
-        // console.log(domains)
-        // console.log(unique_domains) 
-        // document.getElementById("number").innerHTML = "$" + unique_domains; 
-    });
-  console.log('page updated');
-});
 
 chrome.cookies.getAll({domain: ".mydomain.com"}, function(cookies) {
     for(var i=0; i<cookies.length;i++) {
@@ -44,9 +8,3 @@ chrome.cookies.getAll({domain: ".mydomain.com"}, function(cookies) {
       chrome.cookies.remove({url: "https://" + cookies[i].domain  + cookies[i].path, name: cookies[i].name});
     }
   });
-
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
-  if(message.popupOpen) { 
-    console.log("popup open")
-  }
-});
--- popup.js
+++ popup.js
@@ -2,22 +2,12 @@
 if (!chrome.cookies) {
   chrome.cookies = chrome.experimental.cookies;
 }
-chrome.runtime.sendMessage({popupOpen: true});
 
 // when we open the chrome extension...
 chrome.tabs.query({"status":"complete","windowId":chrome.windows.WINDOW_ID_CURRENT,"active":true}, function(tab){
   console.log(JSON.stringify(tab));
   // chrome.cookies.getAll({"url":tab[0].url}, function(cookies) {
   chrome.cookies.getAll({}, function(cookies) {
-    // var domains = [];
-    // all_cookies = "";
-    // var counter = 0
-    // for (var elem in cookies) {
-    //   // var curr = JSON.stringify(cookies[elem]);
-    //   // all_cookies = all_cookies + curr;
-    //   domains.push(cookies[elem].domain);
-    //   counter += 1;
-    // }
     let result = cookies.map(a => a.domain);
     console.log(result)
     new_res = Counter(result);
@@ -25,16 +15,15 @@ chrome.tabs.query({"status":"complete","windowId":chrome.windows.WINDOW_ID_CURRE
 
     var top_3 = Object.fromEntries(Object
           .entries(new_res)
-          .sort(([, a], [, b]) => b - a)                         // just to be sure
+          .sort(([, a], [, b]) => b - a)
           .filter((s => ([, v]) => s.add(v).size <= 3)(new Set))
       );
     
-    top_3_formatted = Object.keys(top_3).map(s => s.slice(1)).join(', ');
+    top_3_formatted = Object.keys(top_3).map(s => s.slice(1)).slice(0, 3).join(', ');
   
-    // console.log(Object.keys(new_res).reduce(function(a, b){ return new_res[a] > new_res[b] ? a : b }));
     // localStorage.all_cookies = all_cookies;
     document.getElementById("number").innerHTML = "$" + cookies.length.toLocaleString();
-    document.getElementById("cookies").innerHTML = "including: " + top_3_formatted;
+    document.getElementById("cookies").innerHTML = "top 3: " + top_3_formatted;
   });
 });
 
@@ -43,5 +32,3 @@ function Counter(array) {
     array.forEach(val => count[val] = (count[val] || 0) + 1);
     return count;
   }
-
-//document.getElementById("number").innerHTML = "$" + (Math.floor(Math.random() * 100 + 1000)).toLocaleString()
